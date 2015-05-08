1d5405b2001415ce9cc7059a640e9e3bc1baa75d
Paul Mateos <mateosau@yahoo.com>
2015-05-08 13:19:52 +1000

    Complete Solution

--- SeleniumNUnitVS2013/UnitTest1.cs
+++ SeleniumNUnitVS2013/UnitTest1.cs
@@ -1,14 +1,47 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace SeleniumNUnitVS2013
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
         {
+            driver = new FirefoxDriver();    
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+
+        }
+        
+        [Test]
+        public void GoogleSearch()
+        {
+
+            driver.Navigate().GoToUrl("http://www.google.com.au");
+            
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Selenium");
+            query.Submit();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until((d) => { return d.Title.StartsWith("Selenium"); });
+
+            Assert.AreEqual("Selenium - Google Search", driver.Title);
+
         }
     }
 }
